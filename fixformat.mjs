933ebddc5decb09e043a24b3f955e769c78247c5
Tempystral <elijah.doern@gmail.com>
2025-02-01 01:38:37 -0500

    Update fixformat script

--- fixformat.mjs
+++ fixformat.mjs
@@ -4,8 +4,9 @@ import { replaceInFile, replaceInFileSync } from "replace-in-file";
 
 const patterns = {
 	newlines: /{{<.*(\n+)?[^{}<>\n]*(\n+)?>}}/gm,
-	figure:
-		/{{<.*figure(?=.*src="(?<name>[^"]+)")(?=.*caption="(?<caption>[^"]+)")(?=.*alt="(?<alt>[^"]+)").*>}}/gm,
+	figureName: /{{<.*figure.*src="(?<name>[^"]+)"(.*)>}}/gm,
+	figureCaption: /<Figure(.*)caption="(?<caption>[^"]+)"(.*)>/gm,
+	figureAlt: /<Figure(.*)alt="(?<alt>[^"]+)"(.*)>/gm,
 	resources: /^.*name:\s*(?<name>.*)$\n^.*src:\s*(?<src>.*)$\n/gm,
 };
 
@@ -14,6 +15,7 @@ try {
 	const resources = [];
 	let results = "";
 
+	// Replace newlines in figures
 	results = await replaceInFile({
 		files: blogposts,
 		from: patterns.newlines,
@@ -31,6 +33,7 @@ try {
 			matches.forEach((m) => resources.push([m.groups.name, m.groups.src]));
 			console.log("Grabbed resources: ", resources);
 
+			// Replace figure name=etc with figure src=etc
 			results = replaceInFileSync({
 				files: path,
 				dry: false,
@@ -44,9 +47,16 @@ try {
 
 	results = await replaceInFile({
 		files: blogposts,
-		from: patterns.figure,
-		to: (match, name, caption, alt) =>
-			`<TFigure src="${name}" alt="${alt}">\n  ${caption}\n</TFigure>`,
+		from: patterns.figureName,
+		to: (match, name, other) => `<Figure src="${name}"${other}>\n</Figure>`,
+		dry: false,
+	});
+
+	results = await replaceInFile({
+		files: blogposts,
+		from: patterns.figureCaption,
+		to: (match, before, caption, after) =>
+			`<Figure ${before.trim()}${after.trim()}>\n${caption}`,
 		dry: false,
 	});
 	console.log("Replacement results:", results);
--- src/components/blog/components.ts
+++ src/components/blog/components.ts
@@ -1,5 +1,6 @@
 import Detail from "./partials/Detail.astro";
 import Figure from "./partials/Figure.astro";
 import Video from "./partials/Video.astro";
+import Youtube from "./partials/Youtube.astro";
 
-export { Detail, Figure, Video };
+export { Detail, Figure, Video, Youtube };
